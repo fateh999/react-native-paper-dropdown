9b2680e34cf64e981818817f8d122651db20cb3b
Fateh Farooqui <farooquifateh184@gmail.com>
2021-07-12 15:30:15 +0530

    Fixes

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,15 +1,18 @@
 import {
   Appbar,
   DarkTheme,
+  DefaultTheme,
   Provider,
   Surface,
   TextInput,
+  ThemeProvider,
 } from 'react-native-paper';
 import React, {Dispatch, SetStateAction, useState} from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import DropDown from 'react-native-paper-dropdown';
 
 function App() {
+  const [nightMode, setNightmode] = useState(false);
   const [showDropDown, setShowDropDown] = useState(false);
   const [gender, setGender]: [
     string | number | undefined,
@@ -31,28 +34,39 @@ function App() {
   ];
 
   return (
-    <Provider theme={DarkTheme}>
-      <StatusBar barStyle={'light-content'} />
-      <Appbar.Header>
-        <Appbar.Content title={'DropDown Demo'} />
-      </Appbar.Header>
-      <Surface style={styles.containerStyle}>
-        <SafeAreaView style={styles.safeContainerStyle}>
-          <DropDown
-            label={'Gender'}
-            mode={'outlined'}
-            visible={showDropDown}
-            showDropDown={() => setShowDropDown(true)}
-            onDismiss={() => setShowDropDown(false)}
-            value={gender}
-            setValue={setGender}
-            list={genderList}
-            inputProps={{
-              right: <TextInput.Icon name={'menu-down'} />,
-            }}
+    <Provider theme={nightMode ? DarkTheme : DefaultTheme}>
+      <ThemeProvider theme={nightMode ? DarkTheme : DefaultTheme}>
+        <StatusBar
+          backgroundColor={
+            nightMode ? DarkTheme.colors.surface : DefaultTheme.colors.primary
+          }
+          barStyle={'light-content'}
+        />
+        <Appbar.Header>
+          <Appbar.Content title="React Native Paper Dropdown" />
+          <Appbar.Action
+            icon={nightMode ? 'brightness-7' : 'brightness-3'}
+            onPress={() => setNightmode(!nightMode)}
           />
-        </SafeAreaView>
-      </Surface>
+        </Appbar.Header>
+        <Surface style={styles.containerStyle}>
+          <SafeAreaView style={styles.safeContainerStyle}>
+            <DropDown
+              label={'Gender'}
+              mode={'outlined'}
+              visible={showDropDown}
+              showDropDown={() => setShowDropDown(true)}
+              onDismiss={() => setShowDropDown(false)}
+              value={gender}
+              setValue={setGender}
+              list={genderList}
+              inputProps={{
+                right: <TextInput.Icon name={'menu-down'} />,
+              }}
+            />
+          </SafeAreaView>
+        </Surface>
+      </ThemeProvider>
     </Provider>
   );
 }
